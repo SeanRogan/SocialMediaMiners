6d387b2a82f5f11eee571f9774f1325e3678cb66
Sean Rogan <74671024+SeanRogan@users.noreply.github.com>
2023-05-20 20:31:48 -0400

    creating files for youtube scraper project

---
+++ YoutubeScraper/post_scraper.py
@@ -0,0 +1,16 @@
+# RESEARCH PROCESS
+# connect to api
+# search for topic
+# collect pages
+# iterate through and collect specific video links
+# save info
+
+# SCRAPER PROCESS
+# connect to api
+# search through saved info
+# scrape page
+# save raw results and send to analysis
+
+# ANALYSIS PROCESS
+# analyze video transcript and extract data
+# analyze comment threads and extract sentiment
